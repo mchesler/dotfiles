1fdd67fde4390ee6b920048557e28025ae6cd6f7
Matt Chesler <mchesler@chesent.com>
2015-04-21 09:30:09 -0400

    Initial machine setup tasks

---
+++ setup/brew.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install bash-completion
+brew install git
+brew install git-flow
+
+brew install ansible
+brew install autoconf
+
+brew install csshx
+brew install curl
+brew install wget
+
+brew install libevent
+brew install libyaml
+brew install openssl
+brew install pcre
+brew install pkg-config
+brew install ssh-copy-id
+brew install the_silver_searcher
+brew install tmux
+brew install xz
+brew install youtube-dl
+
+brew install rbenv
+brew install ruby-build
+rbenv init
+rbenv install 1.9.3-p551
